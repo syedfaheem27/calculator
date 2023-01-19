9ebffe462e8c18fb8639325944e33dbac7eb3c37
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-19 17:36:51 +0530

    Declare variables and save the references of the DOM elements in them.

---
+++ script.js
@@ -0,0 +1,14 @@
+const screen=document.querySelector('.screen');
+
+const clear=document.querySelector('.clear');
+const clear_all=document.querySelector('.clear-all');
+const sign_change=document.querySelector('.sign-change');
+
+const num_list=document.querySelectorAll('.num');
+const decimal=document.querySelector('.decimal');
+const equal_to=document.querySelector('.equal-to');
+
+const add=document.querySelector('.add');
+const subtract=document.querySelector('.subtract');
+const multiply=document.querySelector('.multiply');
+const divide=document.querySelector('.divide');
