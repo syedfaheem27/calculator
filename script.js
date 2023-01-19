38faa43b979ad307a52d2338537f31e6a9b416ae
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-19 18:43:23 +0530

    Add the purpose of each variable being used in the code.

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ const multiply=document.querySelector('.multiply');
 const divide=document.querySelector('.divide');
 
 let cl_flag,operand='',disp='',n1,n2,n3,decimal_count=0,equal_count=1;
+
+//op-denotes the operator pressed
+//n1- used to store the first operand in case of all operators.
+//n2-used to store the second operand in case of addition and subtraction.
+//n3 - used to store the second operand in case of multiplication and division.
+//cl_flag indicates the block of code which was executed just before the current block of code is executed.
+//decimal_count ensures that a decimal button responds only once when pressed simultaneously.
+//equal_count ensures the calculator displays a new set of number if we press any number right after pressing the equal_to button.
+
